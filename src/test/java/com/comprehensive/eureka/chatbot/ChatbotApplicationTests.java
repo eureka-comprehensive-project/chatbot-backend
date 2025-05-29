66ca715418b394918bef8575a3fb288411e37ce5
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-05-29 21:29:42 +0900

    first commit

---
+++ src/test/java/com/comprehensive/eureka/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comprehensive.eureka.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
