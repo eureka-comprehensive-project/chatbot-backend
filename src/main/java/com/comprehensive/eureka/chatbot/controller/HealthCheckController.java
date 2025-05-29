1c20c1a3901ec103e294bf1d2049f536abecaca9
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-05-29 21:37:03 +0900

    feat : healthCheck 컨트롤러 추가

---
+++ src/main/java/com/comprehensive/eureka/chatbot/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.comprehensive.eureka.chatbot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        return "chatbot 25.05.29";
+    }
+}
