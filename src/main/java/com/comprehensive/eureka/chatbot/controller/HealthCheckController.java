9dfb9e89223868e72dc5384b00fe4f1d2bf8ba3b
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-06-01 15:31:35 +0900

    refactor : healthCheck Controller 매핑 주소 추가

--- src/main/java/com/comprehensive/eureka/chatbot/controller/HealthCheckController.java
+++ src/main/java/com/comprehensive/eureka/chatbot/controller/HealthCheckController.java
@@ -1,13 +1,15 @@
 package com.comprehensive.eureka.chatbot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/chatbot")
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/healthCheck")
     public String healthCheck() {
-        return "chatbot 25.05.29";
+        return "chatbot 25.06.01";
     }
 }
