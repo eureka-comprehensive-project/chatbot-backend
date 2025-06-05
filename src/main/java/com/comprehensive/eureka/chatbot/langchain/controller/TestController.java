bf337f97cd603f1599b713ee96c23fea2a3214e4
Siheon Kim <siheon0411@gmail.com>
2025-06-05 17:50:44 +0900

    fix: request mapping에 /chatbot 추가

--- src/main/java/com/comprehensive/eureka/chatbot/langchain/controller/TestController.java
+++ src/main/java/com/comprehensive/eureka/chatbot/langchain/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/hello")
+    @GetMapping("/chatbot/hello")
     public String hello() {
         return "Hello World";
     }
