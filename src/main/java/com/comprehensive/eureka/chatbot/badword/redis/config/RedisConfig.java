6f6b2026ebd58eb33d40c35c2b2262f079b30e25
mike7643 <developerJYumin@gmail.com>
2025-06-15 18:13:57 +0900

    refactor: Redis 프로퍼티 키를 Spring Data Redis 설정에 맞게 업데이트

--- src/main/java/com/comprehensive/eureka/chatbot/badword/redis/config/RedisConfig.java
+++ src/main/java/com/comprehensive/eureka/chatbot/badword/redis/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.comprehensive.eureka.chatbot.badword.redis.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -14,8 +13,8 @@ public class RedisConfig {
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(
-        @Value("${spring.redis.host}") String host,
-        @Value("${spring.redis.port}") int port){
+        @Value("${spring.data.redis.host}") String host,
+        @Value("${spring.data.redis.port}") int port){
 
         RedisStandaloneConfiguration cfg = new RedisStandaloneConfiguration(host, port);
         return new LettuceConnectionFactory(cfg);
