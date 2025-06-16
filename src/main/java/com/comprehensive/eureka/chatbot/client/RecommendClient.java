d2642d03fac91044f81405a84b3240e1ea4e6c2b
mike7643 <developerJYumin@gmail.com>
2025-06-16 15:30:58 +0900

    refactor: 추천 로직 업데이트 및 관련 데이터 처리 구조 개선
    JSON 프롬프트 파일에서 preferenceBenefitGroupId 타입을 문자열에서 정수(Integer)로 변경.
    
    sendToRecommendationModule 메서드가 새로운 RecommendationResponseDto 객체를 반환하도록 수정.
    
    추천 로직을 새로운 응답 구조에 맞게 전반적으로 리팩터링.
    
    로깅을 개선하고 응답 메시지를 일부 수정.
    
    RecommendClient를 변경된 API 명세에 맞춰 업데이트.

--- src/main/java/com/comprehensive/eureka/chatbot/client/RecommendClient.java
+++ src/main/java/com/comprehensive/eureka/chatbot/client/RecommendClient.java
@@ -2,6 +2,7 @@ package com.comprehensive.eureka.chatbot.client;
 
 import com.comprehensive.eureka.chatbot.common.dto.BaseResponseDto;
 import com.comprehensive.eureka.chatbot.langchain.dto.RecommendPlanDto;
+import com.comprehensive.eureka.chatbot.langchain.dto.RecommendationResponseDto;
 import com.comprehensive.eureka.chatbot.langchain.dto.UserPreferenceDto;
 import com.comprehensive.eureka.chatbot.util.WebClientUtil;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +18,21 @@ public class RecommendClient {
     private final WebClientUtil webClientUtil;
 
     // 통신 성향 기반 추천
-    public BaseResponseDto<List<RecommendPlanDto>> recommend(UserPreferenceDto preference, Long userId) {
+    public BaseResponseDto<RecommendationResponseDto> recommend(UserPreferenceDto preference, Long userId) {
         String url = String.format("http://localhost:8083/recommend/%s", userId);
-        log.info("추천 모듈로 환경설정 전송 중: {}, {}", preference, userId);
-        return webClientUtil.post(url, preference, new ParameterizedTypeReference<BaseResponseDto<List<RecommendPlanDto>>>() {});
+//        String url = String.format("https://www.visiblego.com/recommend/%s", userId);
+        log.info("추천 모듈로 환경설정 전송 중: {}", preference);
+        return webClientUtil.post(
+                url,
+                preference,
+                new ParameterizedTypeReference<BaseResponseDto<RecommendationResponseDto>>() {}
+        );
     }
 
     // 키워드 기반 추천
     public BaseResponseDto<List<RecommendPlanDto>> recommendByKeyword(String keyword) {
         String url = String.format("http://localhost:8083/recommend/keyword/%s", keyword);
+//        String url = String.format("https://www.visiblego.com/recommend/keyword/%s", keyword);
         log.info("키워드를 추천 모듈로 전송 중: {}", keyword);
 
         return webClientUtil.<List<RecommendPlanDto>>getWithPathVariable(
