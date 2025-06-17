4cb83d0cb707aabfd23fc5910c8ceea52288e8ad
jmk445 <jmk445123456@gmail.com>
2025-06-17 13:07:26 +0900

    fix : filter삭제

--- src/main/java/com/comprehensive/eureka/chatbot/client/PlanClient.java
+++ src/main/java/com/comprehensive/eureka/chatbot/client/PlanClient.java
@@ -14,8 +14,8 @@ public class PlanClient {
     private final WebClientUtil webClientUtil;
     public Long getBenefitIds(BenefitRequestDto requestDto) {
 
-//        String apiUrl = "http://localhost:8084/plan/benefit/benefit-group";
-        String apiUrl = "https://www.visiblego.com/plan/benefit/benefit-group";
+        String apiUrl = "http://localhost:8084/plan/benefit/benefit-group";
+//        String apiUrl = "https://www.visiblego.com/plan/benefit/benefit-group";
 
         BaseResponseDto<Long> response = webClientUtil.post(
                 apiUrl,
